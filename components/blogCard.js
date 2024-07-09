28b8c685be4d5ac546bdceeb30a35ed2244aa603
snehakhedekar3 <sujatalogicloop@gmail.com>
2024-07-10 02:11:54 +0530

    changes

--- components/blogCard.js
+++ components/blogCard.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 const BlogCard = ({ title, content, image }) => (
-  <div className="card mb-4 border-0">
+  <div className="card border-0">
     <img src={image} alt={title} className="card-img-top" />
     <div className="card-body">
       <h5 className="card-title p-0" style={{ color: "var(--primary-color)" }}>
@@ -12,9 +12,9 @@ const BlogCard = ({ title, content, image }) => (
       <a
         href="#"
         className="subheading"
-        style={{ color: "var(--primary-color)" }}
+       
       >
-        Read More
+      
       </a>
     </div>
   </div>
--- container/Home/blog.js
+++ container/Home/blog.js
@@ -1,29 +1,77 @@
 import React from "react";
-import BlogCard from "../../components/blogCard";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import BlogCard from "@/components/blogCard";
+import Button from "@/components/button";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Navigation } from "swiper/modules";
+
 const BlogData = [
   {
-    title: "Benefits of living in an Integrated...",
+    title: "Benefits of living in an Integrated Township",
     content: "By Dustin Hansen | Nov 29, 2022",
     image: "/image/blog/benefits-rise-blog.png",
   },
   {
-    title: "Investment in Real Estate...",
+    title: "Merlin Rise: A Benchmark of Modern Lifestyle",
     content: "By Dustin Hansen | Nov 29, 2022",
     image: "/image/blog/Rise-beyond-4-walls.png",
   },
+   {
+    title: "Merlin Rise: A Benchmark of Modern Lifestyle",
+    content: "By Dustin Hansen | Nov 29, 2022",
+    image: "/image/blog/Rise-beyond-4-walls.png",
+  },
+  {
+    title: "Merlin Rise: A Benchmark of Modern Lifestyle",
+    content: "By Dustin Hansen | Nov 29, 2022",
+    image: "/image/blog/Rise-beyond-4-walls.png",
+  },
+  // Add more blog data as needed
 ];
 
 const Blog = () => (
-  <div className="container mt-5">
-    <h2 className="text-center mb-5 heading">Blogs</h2>
-    <div className="row">
-      {BlogData.map((article, index) => (
-        <div key={index} className="col-md-6">
-          <BlogCard {...article} />
-        </div>
-      ))}
+  <>
+    <div className="container mt-5">
+      <h2 className="text-center mb-5 heading">Blogs</h2>
+      <Swiper
+          className="swiper-container"
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 50,
+            },
+          }}
+          spaceBetween={5}
+          modules={[Navigation]}
+
+      >
+        {BlogData.map((article, index) => (
+          <SwiperSlide key={index}>
+            <BlogCard {...article} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="explore-more p-4 justify-content-center align-items-center d-flex">
+            <Button text="More Blogs" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default Blog;
